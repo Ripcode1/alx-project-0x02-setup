109c2f6e339abf809a577eeaac1a4ad21ff6c80f
Ripcode1 <161825490+Ripcode1@users.noreply.github.com>
2025-11-23 15:35:03 +0200

    Update index.ts

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,1 +1,48 @@
+export interface CardProps {
+  title: string;
+  content: string;
+}
 
+export interface ButtonProps {
+  size: 'small' | 'medium' | 'large';
+  shape: 'rounded-sm' | 'rounded-md' | 'rounded-full';
+  children: React.ReactNode;
+  onClick?: () => void;
+}
+
+export interface PostProps {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+export interface PostData {
+  title: string;
+  content: string;
+  userId: number;
+}
+
+export interface UserProps {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
